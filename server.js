c6b543a7d1b281cdcbf8886b8c327a5bdbbe1eb4
clement <clement.vanpeuter@gmail.com>
2019-06-26 21:58:54 +0200

    code refacto

--- server.js
+++ server.js
@@ -21,16 +21,15 @@ const checkJwt = jwt({
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.static('./dist'));
 
 app.use('/api', checkJwt, api);
 
-app.use('/plugins/:name', (req, res, next) => {  
+app.use('/plugins/:name', (req, res, next) => {
   if (fs.existsSync(`./plugins/${req.params.name}/index.html`)) {
     const plugin = express.static(`./plugins/${req.params.name}`);
     plugin(req, res, next);
   } else {
-    res.status(501).json({ error: true, msg: 'KO', error_msg: 'This plugin does not exist' });
+    res.status(500).json({ error: true, msg: 'KO', error_msg: 'This plugin does not exist' });
   }
 });
 
@@ -38,10 +37,10 @@ const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
     app.listen(process.env.PORT || 3000, () => {
-      console.log('Example app listening on port 3000!');
+      console.info('Server is listening on port 3000!');
     });
   } catch (error) {
-    console.log('Erreur database connexion');
+    console.warn('Erreur database connexion');
   }
 };
 
